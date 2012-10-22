becadfbdfbe3e82b133b441a1c786cda0d090c19
Joseph Moniz <joseph.moniz@gmail.com>
2012-10-21 17:29:51 -0700

    wrote an echo client tcp example in main

--- src/main.cc
+++ src/main.cc
@@ -7,16 +7,34 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <memory>
 
 #include "loop.h"
 #include "timers.h"
 #include "path.h"
 #include "eventemitter.h"
 #include "querystring.h"
+#include "net/socket.h"
 
 int main(int argc, char **argv) {
-  auto loop = node::Loop::getDefault();
+  auto loop   = node::Loop::getDefault();
+  auto socket = std::make_shared<node::net::Socket>();
 
+  std::cout << "=== Connecting to echo server" << std::endl;
+  socket->connect(8080, "127.0.0.1", [&](){
+    std::cout << "in connect cb" << std::endl << std::endl;
+
+    std::cout << "==== Testing writing from client socket" << std::endl;
+    socket->write("yo dawg\n", [&](){
+      std::cout << "in write cb" << std::endl << std::endl;
+
+      std::cout << "==== Testing reading from client socket" << std::endl;
+      socket->on("data", [&](void *data) {
+          uv_buf_t* buf = (uv_buf_t*)data;
+          std::cout << "<-- " << buf->base << std::endl;
+      });
+    });
+  });
 
   loop->run();
 
